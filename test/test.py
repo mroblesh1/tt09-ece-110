ad38cf912a2f3d11cad25373de46492b98aaef64
Miguel <mroblesh@ucsc.edu>
2024-11-10 01:00:37 -0800

    Finished making tutorial testbench

--- test/test.py
+++ test/test.py
@@ -20,7 +20,7 @@ async def test_project(dut):
     dut.ui_in.value = 0
     await ClockCycles(dut.clk, 10)
 
-    dut.ui_in.value = 20
+    dut.ui_in.value = 110
     await ClockCycles(dut.clk, 100)
 
     # await ClockCycles(dut.clk, 20)
